4cd38ba2cc4942e47210b2350c657427047f5071
LEEHYUNJEE <54610824+hyun-jii@users.noreply.github.com>
2019-10-10 18:08:59 +0900

    Database 연결 테스트

--- JDBC/Test001.java
+++ JDBC/Test001.java
@@ -1,1 +1,30 @@
+/*===============================
+ 		Test001.java
+ 		- Database 연결 테스트
+ ================================*/
 
+package com.test;
+
+import java.sql.Connection;
+
+import com.util.DBConn;
+
+public class Test001
+{
+
+	public static void main(String[] args)
+	{
+		Connection conn = DBConn.getConnection();
+		// ※ DB 연결 과정이 부하가 크기 때문에 
+		//    한 번 연결된 객체를 계속 사용할 수 있도록 Singleton 패턴 적용
+		
+		if(conn != null)
+		{
+			System.out.println("데이터베이스 연결 성공~!!!");
+		}
+		
+		DBConn.close();
+
+	}
+
+}
