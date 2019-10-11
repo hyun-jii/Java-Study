dfde8e0fa2174ca151a3a6790cbb5079eb717887
LEEHYUNJEE <54610824+hyun-jii@users.noreply.github.com>
2019-10-11 18:14:55 +0900

    데이터 저장 및 전송 객체 구성

---
+++ JDBC/MemberDTO.java
@@ -0,0 +1,46 @@
+/*====================================
+ 	MemberDTO.java
+ 	- 单捞磐 历厘 棺 傈价 按眉 备己
+ =====================================*/
+
+package com.test;
+
+public class MemberDTO
+{
+	// 林夸 加己
+	private String sid, name, tel;
+
+	// getter / setter 备己
+	public String getSid()
+	{
+		return sid;
+	}
+
+	public void setSid(String sid)
+	{
+		this.sid = sid;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public String getTel()
+	{
+		return tel;
+	}
+
+	public void setTel(String tel)
+	{
+		this.tel = tel;
+	}
+	
+	
+
+}
